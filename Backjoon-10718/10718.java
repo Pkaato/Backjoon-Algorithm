687d0ec032ec2625107cc29156d95b24759e97ea
ParkCY <42853965+Pkaato@users.noreply.github.com>
2022-10-23 15:24:40 +0900

    Create 10718.java

---
+++ Backjoon-10718/10718.java
@@ -0,0 +1,12 @@
+/**
+ * 2行にわたって「강한친구 대한육군」を1行に1回ずつ出力する。
+ * java clean : Java: Clean the Java language server workspace
+ */
+class m10718 {
+    public static void main(String[] args) {
+        String a = "강한친구 대한육군";
+        for (int i = 0; i < 2; i++) {
+            System.out.println(a);
+        }
+    }
+}
