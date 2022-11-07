20ef261fa9852ae0dc355f548c95316268297d29
ParkCY <42853965+Pkaato@users.noreply.github.com>
2022-11-07 15:07:07 +0900

    Create 10869.java
    https://www.acmicpc.net/problem/10869

---
+++ Backjoon-10869/10869.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+    }
+}
