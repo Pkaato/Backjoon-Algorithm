158dd9f4af3de8f7974a89bd795328837f4fde8d
ParkCY <42853965+Pkaato@users.noreply.github.com>
2022-11-07 14:43:53 +0900

    Create 1008.java
    https://www.acmicpc.net/problem/1008

---
+++ Backjoon-1008/1008.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Double a = sc.nextDouble();
+        Double b = sc.nextDouble();
+        sc.close();
+
+        System.out.println(a / b);
+
+    }
+}
